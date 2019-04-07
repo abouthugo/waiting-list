f0d400496fd3cbdb7164203f697b0d5c81a35daa
Hugo Perdomo <hugo.j.dev@gmail.com>
2019-04-06 22:26:26 -0400

    Initial commit

---
+++ app/src/main/java/info/androidhive/sqlite/view/StudentAdapter.java
@@ -0,0 +1,72 @@
+package info.androidhive.sqlite.view;
+
+/**
+ * Created by ravi on 20/02/18.
+ */
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import info.androidhive.sqlite.R;
+import info.androidhive.sqlite.database.model.Student;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<Student> studentsList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView course;
+        public TextView studentName;
+        public TextView grade;
+
+        public MyViewHolder(View view) {
+            super(view);
+            course = view.findViewById(R.id.student_course);
+            studentName = view.findViewById(R.id.student_dot);
+            grade = view.findViewById(R.id.grade);
+        }
+    }
+
+
+    public StudentAdapter(Context context, List<Student> studentsList) {
+        this.context = context;
+        this.studentsList = studentsList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.student_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Student student = studentsList.get(position);
+
+        holder.course.setText(student.getCourse());
+
+        // Displaying studentName from HTML character code
+        holder.studentName.setText(student.getName());
+
+        // Displaying grade
+        holder.grade.setText(Integer.toString(student.getGrade()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentsList.size();
+    }
+
+}
